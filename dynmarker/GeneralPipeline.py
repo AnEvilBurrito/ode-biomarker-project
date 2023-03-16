f1777dc6ed06d10cdb3cf211a94e018f032557fd
AnEvilBurrito <47137689+AnEvilBurrito@users.noreply.github.com>
2023-03-16 18:06:03 +1100

    update benchmark notebook

--- dynmarker/GeneralPipeline.py
+++ dynmarker/GeneralPipeline.py
@@ -5,6 +5,8 @@ from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 
+import itertools
+
 class GeneralPipeline:
 
     def __init__(self, df_columns):
@@ -18,25 +20,33 @@ class GeneralPipeline:
         self.run_model_func = None
         self.function_set = False
 
+        self.plotting_func = None
+        self.plotting_set = False
+
     def set_function(self, func):
         self.run_model_func = func 
         self.function_set = True
+
+    def set_plotting_function(self, func):
+        self.plotting_func = func
+        self.plotting_set = True
 
     def run_function(self, model_list, iterations, n_cores=1, verbose=0, **kwargs):
 
         # clean the evaluation dataframe
         self.evaluation_df = pd.DataFrame(columns=self.evaluation_df.columns)
 
+        all_ranges = [item for item in kwargs.values() if type(item) == list or type(item) == np.ndarray]
+        all_ranges.insert(0, range(iterations))
 
         if self.function_set == False:
             raise ValueError('EvaluationPipeline: The function to be run has not been set')
         if n_cores == 1:
             # run the function in serial, single core, allows for printing
             for model in model_list:
-                for i in range(iterations):
-                    df_cv = self.run_model_func(i, model, verbose=verbose, **kwargs)
-                    for df in df_cv:
-                        self.evaluation_df = pd.concat((self.evaluation_df, df), ignore_index=True)
+                for xs in itertools.product(*all_ranges):
+                    df = self.run_model_func(model, verbose=verbose, *xs, **kwargs)
+                    pd.concat((self.evaluation_df, df), ignore_index=True)
                 print(f'--- Finished {model.__class__.__name__} using {n_cores} core ---')
         else:
             # run the function in parallel with n_jobs cores
@@ -48,9 +58,13 @@ class GeneralPipeline:
             models = model_list
             eval_list_total = []
             for model in models:
-                evaluation_list = Parallel(n_jobs=n_cores)(delayed(self.run_model_func)(i, model, verbose=0, **kwargs) for i in range(iterations))
+                evaluation_list = Parallel(n_jobs=n_cores)(delayed(self.run_model_func)(model, verbose=0, *xs, **kwargs) for xs in itertools.product(*all_ranges))
                 for evaluation in evaluation_list:
-                    for ev in evaluation:
-                        eval_list_total.append(ev)
+                    eval_list_total.append(evaluation)
                 print(f'--- Finished {model.__class__.__name__} using {n_cores} cores ---')
             self.evaluation_df = pd.concat(eval_list_total, ignore_index=True)
+
+    def run_plot(self):
+        if self.plotting_set == False:
+            raise ValueError('EvaluationPipeline: The plotting function has not been set')
+        self.plotting_func(self.evaluation_df)
