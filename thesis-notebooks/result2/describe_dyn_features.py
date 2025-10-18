690f7e6df0b642a2a6adbb3cb29569eeb737762a
Dawson Lan <47137689+AnEvilBurrito@users.noreply.github.com>
2025-10-18 23:09:45 +1100

    dynamic features - new rsd plots in AN05

---
+++ thesis-notebooks/result2/describe_dyn_features.py
@@ -0,0 +1,84 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+#   kernelspec:
+#     display_name: ode-biomarker-project
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# Jupyter notebook in Jupytext format
+
+# %% [markdown]
+# ## Initialisation
+
+# %%
+import os
+
+path = os.getcwd()
+# find the string 'project' in the path, return index
+index_project = path.find("project")
+# slice the path from the index of 'project' to the end
+project_path = path[: index_project + 7]
+# set the working directory
+os.chdir(project_path)
+print(f"Project path set to: {os.getcwd()}")
+
+# %%
+from PathLoader import PathLoader #noqa: E402
+
+path_loader = PathLoader("data_config.env", "current_user.env")
+
+# %%
+from DataLink import DataLink #noqa: E402
+
+data_link = DataLink(path_loader, "data_codes.csv")
+
+# %%
+folder_name = "ThesisResult2-1-DynamicFeatures"
+exp_id = "v1"
+
+if not os.path.exists(f"{path_loader.get_data_path()}data/results/{folder_name}/{exp_id}"):
+    os.makedirs(f"{path_loader.get_data_path()}data/results/{folder_name}/{exp_id}")
+
+file_save_path = f"{path_loader.get_data_path()}data/results/{folder_name}/{exp_id}/"
+
+# %%
+# Load Proteomics Palbociclib dataset
+loading_code = "goncalves-gdsc-2-Palbociclib-LN_IC50-sin"
+proteomic_feature_data, proteomic_label_data = data_link.get_data_using_code(
+    loading_code
+)
+
+print(f"Proteomic feature data shape: {proteomic_feature_data.shape}")
+print(f"Proteomic label data shape: {proteomic_label_data.shape}")
+
+# %%
+# Data preparation and alignment
+import numpy as np #noqa: E402
+
+# Ensure numeric only
+proteomic_feature_data = proteomic_feature_data.select_dtypes(include=[np.number])
+
+# Align indices
+common_indices = sorted(
+    set(proteomic_feature_data.index) & set(proteomic_label_data.index)
+)
+feature_data = proteomic_feature_data.loc[common_indices]
+label_data = proteomic_label_data.loc[common_indices]
+
+print(f"Final aligned dataset shape: {feature_data.shape}")
+print(f"Final aligned label shape: {label_data.shape}")
+
+# %% [markdown]
+# ## Functions
+
+
